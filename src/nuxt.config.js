b14592b346e0e52c9788e42feba64c286aa4b207
inductor <kela@inductor.me>
2018-11-18 21:05:32 +0900

    fix etc

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -1,7 +1,7 @@
-const pkg = require("./package");
+const pkg = require('./package')
 
 module.exports = {
-  mode: "universal",
+  mode: 'universal',
 
   /*
    ** Headers of the page
@@ -9,27 +9,28 @@ module.exports = {
   head: {
     title: 'Top',
     titleTemplate: '%s - showKs',
-    meta: [{
-        charset: "utf-8"
+    meta: [
+      {
+        charset: 'utf-8'
       },
       {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
       },
       {
-        hid: "description",
-        name: "description",
+        hid: 'description',
+        name: 'description',
         content: pkg.description
       },
       {
-        hid: "og:title",
-        name: "og:title",
-        content: "showKs"
+        hid: 'og:title',
+        name: 'og:title',
+        content: 'showKs'
       },
       {
-        hid: "og:description",
-        name: "og:description",
-        content: "コードを書かないエンジニアでも楽しめるPodcast"
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'Sample portal app for showKs'
       },
       // {
       //   hid: "og:image",
@@ -37,68 +38,69 @@ module.exports = {
       //   content: "https://showks.containerdays.jp/img/icons/ogp.jpg"
       // },
       {
-        hid: "og:image:width",
-        name: "og:image:width",
-        content: "1200"
+        hid: 'og:image:width',
+        name: 'og:image:width',
+        content: '1200'
       },
       {
-        hid: "og:image:height",
-        name: "og:image:height",
-        content: "630"
+        hid: 'og:image:height',
+        name: 'og:image:height',
+        content: '630'
       },
       {
-        hid: "og:url",
-        name: "og:url",
-        content: "https://showks.containerdays.jp/"
+        hid: 'og:url',
+        name: 'og:url',
+        content: 'https://showks.containerdays.jp/'
       },
       {
-        hid: "og:type",
-        name: "og:type",
-        content: "website"
+        hid: 'og:type',
+        name: 'og:type',
+        content: 'website'
       },
       {
-        hid: "og:site_name",
-        name: "og:site_name",
-        content: "showKs"
+        hid: 'og:site_name',
+        name: 'og:site_name',
+        content: 'showKs'
       },
       {
-        hid: "twitter:card",
-        name: "twitter:card",
-        content: "summary_large_image"
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image'
       },
       {
-        hid: "twitter:site",
-        name: "twitter:site",
-        content: "@deploy_am"
+        hid: 'twitter:site',
+        name: 'twitter:site',
+        content: '@containerdaysjp'
       },
       {
-        hid: "twitter:creator",
-        name: "twitter:creator",
-        content: "@deploy_am"
-      },
+        hid: 'twitter:creator',
+        name: 'twitter:creator',
+        content: '@containerdaysjp'
+      }
       // {
       //   hid: "twitter:image",
       //   name: "twitter:image",
       //   content: "https://showks.containerdays.jp/img/icons/ogp.jpg"
       // }
     ],
-    link: [{
-        rel: "icon",
-        type: "image/x-icon",
-        href: "/favicon-16x16.png",
-        sizes: "16x16"
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon-16x16.png',
+        sizes: '16x16'
       },
       {
-        rel: "icon",
-        type: "image/x-icon",
-        href: "/favicon-32x32.png",
-        sizes: "32x32"
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon-32x32.png',
+        sizes: '32x32'
       },
       {
-        rel: "icon",
-        type: "image/x-icon",
-        href: "/favicon-96x96.png",
-        sizes: "96x96"
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon-96x96.png',
+        sizes: '96x96'
       }
     ]
   },
@@ -107,33 +109,25 @@ module.exports = {
    ** Customize the progress-bar color
    */
   loading: {
-    color: "#fff"
+    color: '#fff'
   },
 
   /*
    ** Global CSS
    */
-  css: ["element-ui/lib/theme-chalk/index.css"],
+  css: ['element-ui/lib/theme-chalk/index.css'],
 
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ["@/plugins/element-ui",
-    "@/plugins/font-awesome"
-  ],
+  plugins: ['@/plugins/element-ui'],
 
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    "@nuxtjs/axios",
-    "@nuxtjs/pwa",
-    '@nuxtjs/browserconfig',
-    // ['@nuxtjs/google-tag-manager', {
-    //   id: '',
-    //   scriptURL: 'https://www.googletagmanager.com/gtm.js'
-    // }],
+    '@nuxtjs/axios'
   ],
   /*
    ** Axios module configuration
@@ -149,96 +143,32 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    vendor: [
-      'socket.io-client'
-    ],
-    splitChunks: {
-      layouts: true,
-      pages: true,
-      commons: true
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
     },
-    extend(config, ctx) {}
+    vendor: ['ie-buster']
   },
+  modules: ['@nuxtjs/pwa'],
   workbox: {
     dev: true //開発環境でもPWAできるように
   },
-  // manifest: {
-  //   "name": "showKs",
-  //   "short_name": "showKs",
-  //   "start_url": "https://showks.containerdays.jp/",
-  //   "display": "standalone",
-  //   "background_color": "transparent",
-  //   "theme_color": "#000000",
-  //   "lang": "Japanese",
-  //   "orientation": "portrait",
-  //   "description": "コードを書かないエンジニアでも楽しめるPodcast",
-  //   "scope": "https://showks.containerdays.jp/",
-  //   "icons": [{
-  //     "src": "https://showks.containerdays.jp/img/icons/android-chrome-192x192.png",
-  //     "sizes": "192x192"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/android-chrome-512x512.png",
-  //     "sizes": "512x512"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/android-icon-36x36.png",
-  //     "sizes": "36x36"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/android-icon-48x48.png",
-  //     "sizes": "48x48"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/android-icon-72x72.png",
-  //     "sizes": "72x72"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/android-icon-96x96.png",
-  //     "sizes": "96x96"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/android-icon-144x144.png",
-  //     "sizes": "144x144"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/android-icon-192x192.png",
-  //     "sizes": "192x192"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-57x57.png",
-  //     "sizes": "57x57"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-60x60.png",
-  //     "sizes": "60x60"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-72x72.png",
-  //     "sizes": "72x72"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-76x76.png",
-  //     "sizes": "76x76"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-114x114.png",
-  //     "sizes": "114x114"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-120x120.png",
-  //     "sizes": "120x120"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-144x144.png",
-  //     "sizes": "144x144"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-152x152.png",
-  //     "sizes": "152x152"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-180x180.png",
-  //     "sizes": "180x180"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/apple-icon-precomposed.png",
-  //     "sizes": "192x192"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/ms-icon-70x70.png",
-  //     "sizes": "70x70"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/ms-icon-144x144.png",
-  //     "sizes": "144x144"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/ms-icon-150x150.png",
-  //     "sizes": "150x150"
-  //   }, {
-  //     "src": "https://showks.containerdays.jp/img/icons/ms-icon-310x310.png",
-  //     "sizes": "310x310"
-  //   }]
-  // }
-};
+  manifest: {
+    name: 'showKs Portal',
+    short_name: 'showKs Portal',
+    title: 'showKs Portal',
+    'og:title': 'showKs Portal',
+    description: 'showKs Portal',
+    'og:description': 'showKs Portal',
+    lang: 'ja',
+    theme_color: '#ffffff',
+    background_color: '#ffffff'
+  }
+}
