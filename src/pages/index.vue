e40cebf4010d1331c7e1b4670ee497ed98c45695
inductor <kela@inductor.me>
2018-11-27 23:11:25 +0900

    fix

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,16 @@
 
 <template>
-  <ul>
-    <li
-      v-for="instance in instances"
-      :key="instance.id">
-      {{ instance.id }}
-    </li>
-  </ul>
+  <div class="container">
+    <h1>showKs portal site</h1>
+    <h2>List of alive instances</h2>
+    <ul>
+      <li
+        v-for="instance in instances"
+        :key="instance.id">
+        {{ instance.id }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
