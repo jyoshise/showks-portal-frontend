61b1fb9f58ab44f5a26a9e1ddd51e65c6497c7ad
inductor <kela@inductor.me>
2018-11-28 15:08:59 +0900

    github

--- src/plugins/font-awesome.js
+++ src/plugins/font-awesome.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
-import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import {
+  faFacebook,
+  faTwitter,
+  faGithub
+} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faEnvelope)
 library.add(faUser)
 library.add(faFacebook)
 library.add(faTwitter)
+library.add(faGithub)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
